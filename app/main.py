0db0f9b5e06d02b1ab97c7b7cbf0be386056b461
kjalen <kjalenhansen@gmail.com>
2019-03-03 14:21:35 -0800

    lil bit o clean up

--- app/main.py
+++ app/main.py
@@ -452,13 +452,6 @@ def move():
 
 @bottle.post('/end')
 def end():
-    data = bottle.request.json
-
-    """
-    TODO: If your snake AI was stateful,
-        clean up any stateful objects here.
-    """
-    print(json.dumps(data))
 
     return end_response()
 
